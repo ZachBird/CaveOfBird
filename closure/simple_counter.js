26fca0efe4818d6db4b0999e89ed2636ffd3429d
可囿可无 <546439325@qq.com>
2017-08-09 18:10:41 +0800

    2017.08.09 update

--- closure/simple_counter.js
+++ closure/simple_counter.js
@@ -1,3 +1,29 @@
 function counter(name){
-    var
+    var num = 0;
+    function increasement(){
+        num = num + 1;
+        console.log("Counter Name:" + name + "Number = " + num);
+    }
+    return increasement;//返回的是个变量，若返回函数incresasment(); 会报dogIncreasement() is not a function 的错
 }
+var dogIncreasement = counter("dog");
+var catIncreasement = counter("cat");
+catIncreasement();
+dogIncreasement();
+
+
+function createCounter(counterName)
+{
+    var counter = 0;
+    function increment()
+    {
+        counter = counter + 1;
+        console.log("Number of " + counterName + ": " + counter);
+    }
+    return increment;
+}
+var catCounter = createCounter("cats");
+var dogCounter = createCounter("dogs");
+catCounter(); // Number of cats: 1
+catCounter(); // Number of cats: 2
+dogCounter(); // Number of dogs: 1
